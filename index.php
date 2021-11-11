f774f661761ad644bc0c6b8841a21a2c4269cebe
melisamarisolcharlescharles <43731555+melisamarisolcharlescharles@users.noreply.github.com>
2021-11-10 23:46:08 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contenido">
+        <div class="encabezado">
+            <img src="foto.jpg" alt="imagen de melisa">
+            <h1>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Curriculum Vitae</h1>
+        
+        </div>
+
+        <div class="contenedor">
+            <h2>Datos personales:</h2>
+            <ul style="list-style-type: disc;"  style="text-align: center;">
+                 <li>Nombre: Melisa Marisol Charles Charles</li>
+                 <li>Dirección: Calle Agustín López, Colonia Nuevo Santander</li>
+                 <li>Teléfono: 8342512604</li>
+                 <li>Correo: 1830401@upv.edu.mx</li>
+                 <li>Fecha de Nacimiento: 13 de octubre del 2000</li>
+
+            </ul>
+        </div>
+        
+       
+        
+    <div class="contenedor">
+        <h2>Perfil</h2>
+        <p>Soy estudiante de la Universidad Politécnica de Victoria y estudio en la carrera de Ingeniería en Tecnologías de la información, actualmente curso el noveno cuatrimestre</p>
+    </div>
+  
+    <div class="contenedor">
+        <h2>Experiencia laboral</h2>
+        <p>Realicé mis estancias I y II en el SITE el cual consistían en realizar mantenimiento a equipo de cómputo, testear nodos, identificar nodos del edificio C</p>
+    </div>
+    
+
+    <div class="contenedor">
+         <h2>Educación</h2>
+    <ul>
+        <li>Escuela Secundaria Técnica n°71</li>
+        <li>Instituto Tamaulipeco de Capacitación para el Empleo</li>
+        <li>Universidad Politécnica de Victoria</li>
+    </ul>
+    </div>
+   
+    <div class="contenedor">
+        <h2>Idiomas</h2>
+    <ul>
+        <li>Español</li>
+        <li>Inglés</li>
+    </ul>
+    </div>
+    
+    <div class="contenedor">
+        <h2>Habilidades Computacionales</h2>
+    <ul>
+        <li>Redes</li>
+        <li>Mantenimiento a equipo de cómputo</li>
+    </ul>
+    </div>
+
+    <div class="contenedor">
+        <h2>Referencias</h2>
+    <ul type="disc">
+        <li>Yareli Beatriz Torres Espinosa</li>
+        <ul type="square">
+            <li>8342356478</li>
+            <li>IGNACIO ALLENDE, NÚMERO 302, CALLE 15, C.P. 87000, ZONA CENTRO DE CD. VICTORIA, TAMAULIPAS.</li>
+        </ul>
+       
+    </ul>
+    <ul type="disc">
+        <li>Carlos Eduardo Sanchez Charles</li>
+        <ul type="square">
+            <li>8345697854</li>
+            <li>JOSE NUÑEZ DE CACERES 937 , MORELOS , CIUDAD VICTORIA , TAMS , C.P. 87050.</li>
+        </ul>
+    </ul>
+    </div>
+    
+    <div class="contenedor">
+        <div>
+            <h2 style="text-align: center;">Unidad 1</h2>
+            <ul class = "listaActividades">
+                <li><a href="htdocs/unidad_1/1.4 horario_css_melisa/index.php">Horario</a></li>
+                <li><a href="htdocs/unidad_1/1.6 formulario_melisa_css/index.php">Formularios Web</a></li>
+                <li><a href="htdocs/unidad_1/1.7 Evidencia formativa/index.php">Evidencia Formativa</a></li>
+                <li><a href="htdocs/unidad_1/Evaluación Práctica Unidad 1/index.php">Evaluación Práctica</a></li>
+                <li><a href="htdocs/unidad_1/Recuperación Práctica unidad 1/index.php">Recuperación Práctica</a></li>
+            </ul>
+            
+            <br>
+            <br>
+            <h2 style="text-align: center;">Unidad 2</h2>
+                <ul class = "listaActividades">
+                    <li><a href="htdocs/unidad_2/2.1 Evidencia formativa/index.php">CFE</a></li>
+                    <li><a href="htdocs/unidad_2/2.2 Evidencia formativa/index.php">Desviación Estándar</a></li>
+                    <li><a href="htdocs/unidad_2/2.3 Evidencia formativa/index.php">Calculadora Básica</a></li>
+                    <li><a href="htdocs/unidad_2/2.4 Evidencia formativa Calculador IMC con JS/index.php">Calcular IMC</a></li>
+                    <li><a href="htdocs/unidad_2/2.5 Calculadora aritmética/index.php">Calcular Aritmética</a></li>
+                    <li><a href="htdocs/unidad_2/2.6 Evidencia formativa Carrusel de imagenes/index.php">Carrusel de imágenes</a></li>
+                </ul>
+            <br>
+            <br>
+            <h2 style="text-align: center;">Unidad 3</h2>
+                <ul class = "listaActividades">
+                    <li><a href="htdocs/Unidad 3/3.1 Calculo de IMC con PHP/index.php">Calcular IMC con PHP</a></li>
+                    <li><a href="Unidad 3/roux_academy/index.php">Include y Requiere</a></li>
+                    <li><a href="roux_academy/index.php">Proyecto final</a></li>
+                </ul>
+        </div>
+        <br><br>
+
+        <div></h2></div>
+
+    </div>
+
+    <div class="contenedor" style="text-align: center;">
+        <h2>Redes sociales</h2>
+    
+        <a target="_blank" href="https://www.facebook.com/melisa.marisol.9/"> <img width="5%" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="facebook"></a>
+       
+        <a target="_blank" href="https://www.instagram.com/melisamarisol/?hl=es-la"> <img  width="5%" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" alt="instagram"> </a>
+     
+    </div>
+    <br>
+    
+   
+    </div>
+    <br>
+</body>
+</html>
